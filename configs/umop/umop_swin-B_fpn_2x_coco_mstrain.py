af7fa71f9cc244544f49e31e0e86c9497e332454
zimoqingfeng <1072149130@qq.com>
2021-09-14 12:32:32 +0800

    Methods refactoring.
    Change-Id: I780846c5db257cc94dffa440a5d07b8295fa3826

--- configs/umop/umop_swin-B_fpn_2x_coco_mstrain.py
+++ configs/umop/umop_swin-B_fpn_2x_coco_mstrain.py
@@ -5,6 +5,7 @@ model = dict(
         # SwinTransformer-Base
         type='SwinTransformer',
         embed_dim=128,
-        num_heads=[4, 8, 16, 32]),
+        num_heads=[4, 8, 16, 32],
+        pretrain_model_path='./pretrained_backbones/swin_base_patch4_window7_224_22k.pth'),
     neck=dict(
         in_channels=[128, 256, 512, 1024]))
--- configs/umop/umop_swin-L_fpn_2x_coco_mstrain.py
+++ configs/umop/umop_swin-L_fpn_2x_coco_mstrain.py
@@ -5,6 +5,7 @@ model = dict(
         # SwinTransformer-Large
         type='SwinTransformer',
         embed_dim=192,
-        num_heads=[6, 12, 24, 48]),
+        num_heads=[6, 12, 24, 48],
+        pretrain_model_path='./pretrained_backbones/swin_large_patch4_window7_224_22k.pth'),
     neck=dict(
         in_channels=[192, 384, 768, 1536]))
